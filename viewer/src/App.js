735c98bfedc563cc9cf241ee964c37b087bb564a
John Stoecker <johnstoecker@gmail.com>
2017-08-25 17:32:34 -0700

    add bootstrap

--- viewer/src/App.js
+++ viewer/src/App.js
@@ -13,6 +13,9 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <div>
+          <button type="button" className="btn btn-primary">Primary</button>
+        </div>
       </div>
     );
   }
