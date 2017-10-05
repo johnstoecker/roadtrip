affb082e038b295351d5489a18a84f27f076831d
John Stoecker <johnstoecker@gmail.com>
2017-10-04 22:37:56 -0700

    save pixel coords

--- server/ironmaps_server.js
+++ server/ironmaps_server.js
@@ -1,28 +1,29 @@
-//Lets require/import the HTTP module
-var http = require('http');
-var request = require('request');
+var http = require('express');
 var mongo = require('mongodb');
-const db = require('monk')('localhost/ironmapsdb')
+const MongoClient = require('mongodb').MongoClient
 
 //Lets define a port we want to listen to
 const PORT=5001;
 
-//We need a function which handles requests and send response
-function handleRequest(req, response){
-
-  response.writeHead(200, {"Access-Control-Allow-Origin": "*", "Access-Control-Allow-Headers" : "Origin, X-Requested-With, Content-Type, Accept"});
-
-  var collection = db.get('tweets');
-  collection.find({},{},function(e,docs){
-    res.render(docs);
-  });
-}
-
 //Create a server
-var server = http.createServer(handleRequest);
+const express = require('express');
+const app = express();
 
-//Lets start our server
-server.listen(PORT, function(){
-    //Callback triggered when server is successfully listening. Hurray!
+app.get('/', (req, res) => {
+  res.set({"Access-Control-Allow-Origin": "*", "Access-Control-Allow-Headers" : "Origin, X-Requested-With, Content-Type, Accept"});
+  db.collection('tweets').find().toArray((err, result) => {
+    if (err) return console.log(err)
+    // renders index.ejs
+    res.send( {tweets: result})
+  })
+})
+
+var db;
+
+MongoClient.connect('mongodb://localhost:27017/ironmapsdb', (err, database) => {
+  if (err) return console.log(err)
+  db = database
+  app.listen(PORT, () => {
     console.log("Server listening on: http://localhost:%s", PORT);
-});
+  })
+})
--- server/scraper.py
+++ server/scraper.py
@@ -3,35 +3,50 @@ import tweepy
 import json
 from pymongo import MongoClient
 
-MONGO_HOST= 'mongodb://localhost/ironmapsdb'  # assuming you have mongoDB installed locally
-                                             # and a database called 'twitterdb'
+import json
 
-CONSUMER_KEY = "KEY"
-CONSUMER_SECRET = "SECRET"
-ACCESS_TOKEN = "TOKEN"
-ACCESS_TOKEN_SECRET = "TOKEN_SECRET"
+with open('pixel_map.json') as json_data:
+    pixelMap = json.load(json_data)
 
+    MONGO_HOST= 'mongodb://localhost/ironmapsdb'  # assuming you have mongoDB installed locally
+                                                 # and a database called 'twitterdb'
 
-client = MongoClient(MONGO_HOST)
-
-# Use twitterdb database. If it doesn't exist, it will be created.
-db = client.ironmapsdb
-
-auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
-auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
+    CONSUMER_KEY = "KEY"
+    CONSUMER_SECRET = "SECRET"
+    ACCESS_TOKEN = "TOKEN"
+    ACCESS_TOKEN_SECRET = "TOKEN_SECRET"
 
 
-cursor = db.tweets.find().limit(1).sort([("id", -1)])
+    client = MongoClient(MONGO_HOST)
 
-api = tweepy.API(auth)
+    # Use twitterdb database. If it doesn't exist, it will be created.
+    db = client.ironmapsdb
 
-try:
-    record = cursor.next()
-    new_tweets = api.home_timeline(since_id=record.id,count=200)
-except StopIteration:
-    new_tweets = api.home_timeline(count=200)
+    auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+    auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
 
-for s in new_tweets:
-    if db.tweets.find_one({'text':s.text}) == None: # prevent duplicate tweets being stored
-        tweet_to_save = {'text':s.text, 'id':s.id, 'created_at':s.created_at,'screen_name':s.author.screen_name,'author_id':s.author.id, 'geo':s.geo, 'coordinates':s.coordinates}
-        db.tweets.save(tweet_to_save)
+
+    cursor = db.tweets.find().limit(1).sort([("id", -1)])
+
+    api = tweepy.API(auth)
+
+    try:
+        record = cursor.next()
+        new_tweets = api.home_timeline(since_id=record.id,count=200)
+    except StopIteration:
+        new_tweets = api.home_timeline(count=200)
+
+    for s in new_tweets:
+        if db.tweets.find_one({'text':s.text}) == None: # prevent duplicate tweets being stored
+            if s.coordinates:
+                coords = s.coordinates.coordinates
+                # // top left coords in pixelMap
+                # // tweets are in [long, lat]
+                for pm in pixelMap:
+                #     -122.14310264,37.05701649
+                #    "longitude":-122.4992,"latitude":37.6336
+                    if(pm["latitude"]>coords[1] && pm["longitude"]<coords[0] && pm["latitude"]-0.81<coords[1] && pm["longitude"]+0.81>coords[0]):
+                        s.pixel_coords = pixelMap[i]["coords"]
+
+            tweet_to_save = {'text':s.text, 'id':s.id, 'created_at':s.created_at,'screen_name':s.author.screen_name,'author_id':s.author.id, 'geo':s.geo, 'coordinates':s.coordinates, 'pixel_coords': s.pixel_coords}
+            db.tweets.save(tweet_to_save)
