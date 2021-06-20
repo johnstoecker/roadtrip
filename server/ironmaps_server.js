ebd09f28db40af0be21ba1c49d8be8d6b24eecdb
John Stoecker <johnstoecker@gmail.com>
2021-06-20 14:54:18 -0700

    2021 update

--- server/ironmaps_server.js
+++ server/ironmaps_server.js
@@ -18,6 +18,30 @@ app.get('/', (req, res) => {
   })
 })
 
+app.get('/2012', (req, res) => {
+  res.set({"Access-Control-Allow-Origin": "*", "Access-Control-Allow-Headers" : "Origin, X-Requested-With, Content-Type, Accept"});
+  db.collection('tweets')..find({"created_at" : { $gte : new ISODate("2021-01-01T20:15:31Z") }});.toArray((err, result) => {
+    if (err) return console.log(err)
+    // renders index.ejs
+    res.send( {tweets: result})
+  })
+})
+
+app.post('/pixels', (req, res) => {
+  db.collection('pixels').insertOne(req.body)
+    .then(result => {
+      console.log(result)
+    })
+    .catch(error => console.error(error))
+})
+
+app.get('/trips/:id', (req, res) => {
+    db.collection('pixels').find({tripId: req.params.id}).toArray((err, result) => {
+      if (err) return console.log(err)
+      res.send( { pixels: result})
+    })
+});
+
 var db;
 
 MongoClient.connect('mongodb://localhost:27017/ironmapsdb', (err, database) => {
